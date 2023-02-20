1185d99705c7f84b27f140b13d459db4b6b7e37a
Abhi <abhiram.sathiraju@gmail.com>
2023-02-20 15:09:20 +0530

    commit :)

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn JavaScript
+          Learn Anything
         </a>
       </header>
     </div>
