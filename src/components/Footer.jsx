1237fd5a4080f56f390fcae452476a655674a672
gurjaspreet <107759543+gurjaspreet@users.noreply.github.com>
2023-05-16 19:00:12 +0530

    Final commit :)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import pianoBG from '../audio/piano-bg.wav'
 import "../styles/Footer.css"
+
+
 function Footer() {
+  const [music, setToggle] = useState(true)
+  const audio = new Audio(pianoBG);
+  audio.volume = 0.2
+
+  // const playMusic = () => {
+  //   // setToggle(!music)
+  //   if(music) {
+  //     audio.play();
+  //     setToggle(false);
+      
+  //   } else {
+  //     audio.pause();
+  //     setToggle(true);
+  //   }
+  // }
+
+
+ const toggle = () =>{
+  return audio.paused? audio.play() : audio.pause();
+ }
+
+ 
+
+  
+
   return (
     <div>
       <footer>
         <div className='clg'>
             <h1> <a href="https://www.cuchd.in/"  target="_blank">CHANDIGARH UNIVERSITY</a></h1>
         </div>
-        <div className='branch'> CSE</div>
+        <div className='branch'>
+          <button id="bgm" onClick={toggle}>
+            {audio.played ? "🔊":"🔇" }
+          </button>
+        </div>
+        <div className='branch'>CSE</div>
       </footer>
     </div>
   )
